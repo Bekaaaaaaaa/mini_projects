80872f2cd788d4ec50c16582cc170d0d61d35e10
Beka <bkrvbeka@gmail.com>
2023-02-28 12:29:08 +0600

    create read

---
+++ shop/products.py
@@ -0,0 +1,26 @@
+from utils import get_products, update_products
+
+def create():
+    title = input("Введите название: ")
+    price = float(input("Введите цену: "))
+
+    new_product = {"title":title, "price":price}
+    #собираем в словарь
+    products = get_products()
+    # получаем список старых продуктов
+    products.append(new_product)
+    # добавляем в список новые данные
+    update_products(products)
+    # перезаписываем файд с новыми данными
+
+def read():
+    products = get_products()
+    # получаем список продуктов
+    for product in products:
+        # проходимся по каждому продукту и принтим
+        print(f"""
+=================================================
+Название: {product['title']}
+Цена: {product['price']}
+=================================================
+""")
